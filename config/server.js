203fa8faea64f33ab7077ff373a2cc144cbb8a48
Ratama Adhi Nugraha <ratama.adhi@gmail.com>
2021-08-18 21:35:23 +0700

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2d775282358bac12c40958288c1d8202'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8169b2f9-c069-4cb3-9954-6161f87a3a98'
+};
