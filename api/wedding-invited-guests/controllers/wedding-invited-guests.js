b3e21d6bbdbd72d61a89ffe670747d85b8dfbb55
Ratama Adhi Nugraha <ratama.adhi@gmail.com>
2023-12-23 00:23:21 +0700

    feat(wedding): add collection wedding & wedding invited guest

---
+++ api/wedding-invited-guests/controllers/wedding-invited-guests.js
@@ -0,0 +1,22 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services[`wedding-invited-guests`].findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models[`wedding-invited-guests`] });
+  },
+};
